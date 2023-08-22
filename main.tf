892d0fa73fb35ea0df0479eb64c34456ba85b18c
Arshad Ahmad <133496386+howdycloudyarsh@users.noreply.github.com>
2023-08-22 22:51:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,4 @@ resource "aws_key_pair" "example_keypair" {
 resource "aws_instance" "example_instance" {
   ami           = "ami-08e5424edfe926b43"  # Ubuntu 20.04 LTS AMI ID
   instance_type = "t2.micro"  # Change this to the desired instance type
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-
   key_name = aws_key_pair.example_keypair.key_name
