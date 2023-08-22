3df9c0121701766c9ea46351250f489ec951454a
Arshad Ahmad <133496386+howdycloudyarsh@users.noreply.github.com>
2023-08-22 23:39:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,5 +47,10 @@ resource "aws_security_group" "example" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from any IP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from any IP
   }
 }
