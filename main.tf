ffd1b7f04e45b2df08d4768b0414f0415e024196
Arshad Ahmad <133496386+howdycloudyarsh@users.noreply.github.com>
2023-08-22 22:53:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "ap-south-1"  # Mumbai_region
 }
 
-resource "aws_key_pair" "example_keypair" {
-  key_name   = "example-keypair"  # Change this to your desired key pair name
+resource "aws_key_pair" "rajkey" {
+  key_name   = "rajkey"  # Change this to your desired key pair name
   public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key
 }
 
-resource "aws_instance" "example_instance" {
+resource "aws_instance" "raj" {
   ami           = "ami-08e5424edfe926b43"  # Ubuntu 20.04 LTS AMI ID
   instance_type = "t2.micro"  # Change this to the desired instance type
-  key_name = aws_key_pair.example_keypair.key_name
+  key_name = aws_key_pair.rajkey.key_name
