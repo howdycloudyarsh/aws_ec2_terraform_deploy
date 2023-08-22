7abf408844ff8e51048a64be1b7e5de4ce4b416c
Arshad Ahmad <133496386+howdycloudyarsh@users.noreply.github.com>
2023-08-22 15:53:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,19 @@ provider "aws" {
   region = "ap-south-1"  # Mumbai_region
 }
 
-resource "aws_instance" "nginx" {
+resource "aws_instance" "nginx1" {
   ami           = "ami-08e5424edfe926b43"  # Ubuntu_server_20.04
   instance_type = "t2.micro"
 
   tags = {
-    Name = "nginx"
+    Name = "nginx1"
+  }
+
+connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")  # Path to your SSH private key
+    host        = self.public_ip
   }
 
 provisioner "remote-exec" {
