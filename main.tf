2ea5796988e8391bced096f28ba707bcb071f763
Arshad Ahmad <133496386+howdycloudyarsh@users.noreply.github.com>
2023-08-22 15:27:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,20 @@ provider "aws" {
   region = "ap-south-1"  # Mumbai_region
 }
 
-resource "aws_instance" "EC2_DEPLOY" {
+resource "aws_instance" "nginx" {
   ami           = "ami-08e5424edfe926b43"  # Ubuntu_server_20.04
   instance_type = "t2.micro"
 
   tags = {
-    Name = "EC2_DEPLOY"
+    Name = "nginx"
   }
 
 provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install -y nginx",
+      "sudo apt-get install nginx -y",
+      "sudo systemctl enable nginx",
+      "sudo systemctl start nginx"
     ]
   }
 }
